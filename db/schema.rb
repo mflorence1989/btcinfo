3e5cc6cd03e5168e8c9175c538593cbc2e5a6fda
Mario Florence <mflorence1989@gmail.com>
2017-09-23 19:30:40 -0400

    created article.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20170923230811) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
