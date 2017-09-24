51aa7bab4b85cf6f6f54b5bb88cb4b5bfa741c0e
Mario Florence <mflorence1989@gmail.com>
2017-09-24 14:37:39 -0400

    added username and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923234521) do
+ActiveRecord::Schema.define(version: 20170924183316) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170923234521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "email"
+    t.string "username"
   end
 
 end
