8b9606412a56e1cc511df02d0341b4a3b7400d80
Mario Florence <mflorence1989@gmail.com>
2017-09-23 21:54:32 -0400

    created a delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,6 +47,13 @@ def show
   @article = Article.find(params[:id])
 end
 
+def destroy
+@article = Article.find(params[:id])
+@article.destroy
+flash[:notice] = "Post was successfully deleted"
+redirect_to articles_path
+end
+
   private
 
   def article_params
