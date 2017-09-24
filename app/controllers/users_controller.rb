727ece7bac67a46e9647c534141820c712ed12e7
Mario Florence <mflorence1989@gmail.com>
2017-09-24 18:47:41 -0400

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
  def user_params
 
- params.require(:user).permit(:username, :email, :password)
+ params.require(:user).permit(  :username, :email, :password)
 
  end
 
