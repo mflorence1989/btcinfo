c75636f2cfc6be1030173519c0972c19f9e51470
Mario Florence <mflorence1989@gmail.com>
2017-09-24 16:46:00 -0400

    trying to get a signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
+  def new
 
-end
+ @user = User.new
+
+ end
+
+ def create
+
+ @user = User.new(user_params)
+
+ if @user.save
+
+ flash[:success] = "Welcome to CryptoNet #{@user.username}"
+
+ redirect_to articles_path
+
+ else
+
+ render 'new'
+
+ end
+
+ end
+
+ private
+
+ def user_params
+
+ params.require(:user).permit(:username, :email, :password)
+
+ end
+
+ end
