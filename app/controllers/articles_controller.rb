8abf878c70820d7020b4f489032c47547b95c09b
Mario Florence <mflorence1989@gmail.com>
2017-09-23 20:30:08 -0400

    added some code to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,19 @@ class ArticlesController < ApplicationController
 
   end
 
+  def create
+    #render plain: params[:articles].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 
 end
