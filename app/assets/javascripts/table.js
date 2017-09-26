cd8a9c688a56cd8fc612a88ae902b2a2a2b0941c
Mario Florence <mflorence1989@gmail.com>
2017-09-26 16:50:20 -0400

    routes work now

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,28 +1,29 @@
-$(document).ready(function() {
-      $.getJSON("https://blockchain.info/ticker?cors=true", function(json) {
-        var html = "";
-        var usd = "";
-        var gbp = "";
-        var aud = "";
+$(document).ready(function() {  
+  console.log('document is ready')
 
-        html = '<table class="table"><thead><tr><th>Currency</th><th>Price</th></thead><tbody>';
-        jQuery.each(json, function(key, val) {
-          html += "<tr><td><strong>" + key + "</strong></td><td>" + val["last"] + "</td></tr>"
+}
+  $.getJSON("https://blockchain.info/ticker?cors=true", function(json) {
+    var html = "";
+    var usd = "";
+    var gbp = "";
+    var aud = "";
 
-          if (key == "USD"){
-            usd += val["last"] + " " + key
-          }
-          else if (key == "GBP"){
-            gbp += val["last"] + " " + key
-          }
-          else if (key == "AUD"){
-            aud += val["last"] + " " + key
-          }
-        });
-        html += "</tbody></table>";
-        $(".prices").html(html);
-        $(".usd-price").html(usd);
-        $(".gbp-price").html(gbp);
-        $(".aud-price").html(aud);
+    html = '<table class="table"><thead><tr><th>Currency</th><th>Price</th></thead><tbody>';
+    jQuery.each(json, function(key, val) {
+      html += "<tr><td><strong>" + key + "</strong></td><td>" + val["last"] + "</td></tr>"
+
+      if (key == "USD") {
+        usd += val["last"] + " " + key
+      } else if (key == "GBP") {
+        gbp += val["last"] + " " + key
+      } else if (key == "CAD") {
+        aud += val["last"] + " " + key
+      }
     });
+    html += "</tbody></table>";
+    $(".prices").html(html);
+    $(".usd-price").html(usd);
+    $(".gbp-price").html(gbp);
+    $(".aud-price").html(aud);
+  });
 });
